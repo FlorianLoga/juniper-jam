58e5c893fe3d9b02515cd2e8a53e11bfa483e444
florianloga <florian.motloga@gmail.com>
2026-06-26 03:03:18 +0300

    Main Menu + AudioManager + UI

---
+++ Autoloads/AudioManager.gd
@@ -0,0 +1,49 @@
+extends Node
+
+var _master_bus : int
+var _music_bus : int
+var _sfx_bus : int
+
+var master_volume : float = 1.0
+var music_volume : float = 1.0
+var sfx_volume : float = 1.0
+var music_player: AudioStreamPlayer
+
+func _ready() -> void:
+	music_player = AudioStreamPlayer.new()
+	music_player.bus = "Music"
+	add_child(music_player)
+	_master_bus = AudioServer.get_bus_index("Master")
+	_music_bus  = AudioServer.get_bus_index("Music")
+	_sfx_bus    = AudioServer.get_bus_index("Sfx")
+	#music_player.play() 
+
+func set_master_volume(value: float) -> void:
+	master_volume = value
+	AudioServer.set_bus_volume_db(_master_bus, linear_to_db(value))
+
+func set_music_volume(value: float) -> void:
+	music_volume = value
+	AudioServer.set_bus_volume_db(_music_bus, linear_to_db(value))
+
+func set_sfx_volume(value: float) -> void:
+	sfx_volume = value
+	AudioServer.set_bus_volume_db(_sfx_bus, linear_to_db(value))
+
+func get_master_volume() -> float:
+	return master_volume
+
+func get_music_volume() -> float:
+	return music_volume
+
+func get_sfx_volume() -> float:
+	return sfx_volume
+	
+func play_music(stream: AudioStream) -> void:
+	music_player.stream = stream
+	music_player.play()
+	
+	
+func stop_music() -> void:
+	music_player.stop()
+	
---
+++ Scripts/UI/main_menu.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+@export var start_scene_path : String
+@onready var options_panel = $OptionsPanel
+
+func _ready() -> void:
+	var stream = preload("res://Audio/CriticalTheme.wav")
+	AudioManager.play_music(stream)
+	
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file(start_scene_path)
+
+func _on_options_pressed() -> void:
+	options_panel.visible = true
+	
+func _on_quit_pressed() -> void:
+	get_tree().quit()
